8ae1f85875fb42e3be4e986106a502a107a5198f
Admin <admin@mac-studio02s-Mac-Studio.local>
2026-01-09 20:30:10 +0200

    Update site

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
       {
         src: "/cabaret_teleutaia_stigmi_athens_conservatoire_drama_school-8.avif",
         title: "Cabaret Atmosphere",
-        mobilePosition: "center 50%",
+        mobilePosition: "80% 20%",
       },
       {
         src: "/o_vasiikos_theatro_simeio-18.jpg",
